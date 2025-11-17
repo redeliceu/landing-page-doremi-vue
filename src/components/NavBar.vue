09f772aef32de5aecda8c5a9c38f64ae07e5decd
samuel <samuelgomes101112@gmail.com>
2025-11-17 16:01:37 -0300

    add number rigth

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,7 +10,7 @@
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 
                 <a class="text-white bg-laranja-escuro focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg px-4 py-2 text-center"
-                    href="https://wa.me/5511944661190">
+                    href="https://wa.me/5511934992247">
                     Contato
                   </a>
 
